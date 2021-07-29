1f164cc43589b206da4276b0f13d198b7827e1a3
Gloria Edem <gloedem@gmail.com>
2021-07-29 00:55:00 +0000

    slight cleanup

--- fetcher.js
+++ fetcher.js
@@ -1,27 +1,29 @@
 const request = require("request");
 const fs = require("fs");
 const args = process.argv.slice(2);
-// console.log(args);
 
 const fetcher = (url, path) => {
   url = args[0];
   path = args[1];
   let filesize;
   request(url, (error, response, body) => {
-    console.error("error msg: ", error);
     console.log("statusCode: ", response && response.statusCode);
+    if (error) {
+      console.log("❌Invalid URL❌");
+      return error;
+    }
     if (response.statusCode !== 200) {
       console.log("Invalid status code, no file write occured");
       return `${response && response.statusCode} Invalid status code`;
     }
-    //console.log("body: ", body);
     fs.writeFile(path, body, { flag: "wx" }, (err) => {
       if (err) {
+        console.log("❌fs.Write error❌");
         console.error(err);
         return;
       }
       filesize = fs.statSync(path);
-      console.log(`Downloaded ${filesize.size} bytes of data to ${path}`);
+      console.log(`✅Downloaded ${filesize.size} bytes of data to ${path}`);
     });
   });
 };
