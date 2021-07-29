f1f10a61a4afdd6473d10f84a16d2d37a7771a04
Gloria Edem <gloedem@gmail.com>
2021-07-29 00:23:45 +0000

    successfully implemented base case of page-fetcher node app

---
+++ fetcher.js
@@ -0,0 +1,25 @@
+const request = require("request");
+const fs = require("fs");
+const args = process.argv.slice(2);
+// console.log(args);
+
+const fetcher = (url, path) => {
+  url = args[0];
+  path = args[1];
+  let filesize;
+  request(url, (error, response, body) => {
+    console.error("error msg: ", error);
+    console.log("statusCode: ", response && response.statusCode);
+    //console.log("body: ", body);
+    fs.writeFile(path, body, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      filesize = fs.statSync(path);
+      console.log(`Downloaded ${filesize.size} bytes of data to ${path}`);
+    });
+  });
+};
+
+fetcher(args);
